fd46c9a4643c1260167f5b3b9a4230f92bdc54c8
FarzamAlam <farzamcse@gmail.com>
2020-03-27 18:23:25 +0530

    Broke main() into multiple funcs and added filter to show only results of same host

--- go-sitemap-builder/main.go
+++ go-sitemap-builder/main.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
+
+	"github.com/farzamalam/gopher-exercises/go-link-parser/links"
 )
 
 func main() {
@@ -13,12 +16,50 @@ func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "the url to make sitemap for.")
 	flag.Parse()
 	fmt.Println(*urlFlag)
-	// Call a GET request on the url and copy the resp body on os.Stdout and close the resp.
-	resp, err := http.Get(*urlFlag)
+
+	hrefs := get(*urlFlag)
+
+	for i, href := range hrefs {
+		fmt.Printf("%d :  %s\n", i, href)
+	}
+}
+
+func get(urlStr string) []string {
+	resp, err := http.Get(urlStr)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 
-	io.Copy(os.Stdout, resp.Body)
+	reqURL := resp.Request.URL
+	baseURL := &url.URL{
+		Scheme: reqURL.Scheme,
+		Host:   reqURL.Host,
+	}
+	base := baseURL.String()
+	return filter(base, href(resp.Body, base))
+}
+
+func href(r io.Reader, base string) []string {
+	var res []string
+	links, _ := links.Parse(r)
+	for _, link := range links {
+		switch {
+		case strings.HasPrefix(link.Href, "/"):
+			res = append(res, base+link.Href)
+		case strings.HasPrefix(link.Href, "http"):
+			res = append(res, link.Href)
+		}
+	}
+	return res
+}
+
+func filter(base string, hrefs []string) []string {
+	var filterHref []string
+	for _, href := range hrefs {
+		if strings.HasPrefix(href, base) {
+			filterHref = append(filterHref, href)
+		}
+	}
+	return filterHref
 }
