cfdbb4e23ef388229cde372b0439f157388b0a05
FarzamAlam <farzamcse@gmail.com>
2020-03-27 11:22:24 +0530

    Implemented go-link-parser, and wrote comments for all the funcs and types

--- go-link-parser/links/parse.go
+++ go-link-parser/links/parse.go
@@ -1,18 +1,25 @@
 package links
 
 import (
-	"fmt"
 	"io"
 	"strings"
 
 	"golang.org/x/net/html"
 )
 
+// Link is the type that is used to represent the result.
+// The result that is sent back to caller is the slice of Link.
+// It has two fields Href for href and Text for text
 type Link struct {
 	Href string
 	Text string
 }
 
+// Parse is the only exported function, it returns slice of Link or error.
+// First of all it parses the io.Reader into html document using html lib.
+// then it call linkNodes with doc, it is used to return all the <a> nodes.
+// with each node from the linkNodes  calls the buildLink which parses the links and returns link which
+// is appended to links.
 func Parse(r io.Reader) ([]Link, error) {
 	doc, err := html.Parse(r)
 	if err != nil {
@@ -26,6 +33,10 @@ func Parse(r io.Reader) ([]Link, error) {
 	return links, nil
 }
 
+// text is used to take *html.Node tag and returns the text that is present inside it.
+// It uses recursion get data even from inside tags.
+// It looks for textNode, if it found then it returns otherwise it goes inside the child tags recursively.
+// At last it takes the string and breaks into slice and takes a slice with " " and joins them and returns.
 func text(n *html.Node) string {
 	if n.Type == html.TextNode {
 		return n.Data
@@ -41,6 +52,9 @@ func text(n *html.Node) string {
 	return strings.Join(strings.Fields(txt), " ")
 }
 
+// buildLink is called from the Parse. It takes all the <a> tag one by one and
+// finds the attributes where key is "href", if href is found it adds it to link, with value.
+// then it calls the text() with current node and adds the result to current link.Text field.
 func buildLink(n *html.Node) Link {
 	var link Link
 	for _, attr := range n.Attr {
@@ -53,6 +67,8 @@ func buildLink(n *html.Node) Link {
 	return link
 }
 
+//linkNodes is called from the parse, it returns the slice of *htmlNode where n.Data == "a"
+// It uses recurssion to go inside the child nodes.
 func linkNodes(n *html.Node) []*html.Node {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		return []*html.Node{n}
@@ -62,15 +78,4 @@ func linkNodes(n *html.Node) []*html.Node {
 		ret = append(ret, linkNodes(c)...)
 	}
 	return ret
-}
-
-func dfs(n *html.Node, padding string) {
-	msg := n.Data
-	if n.Type == html.ElementNode {
-		msg = "<" + msg + ">"
-	}
-	fmt.Println(padding, msg)
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		dfs(c, padding+"  ")
-	}
 }
