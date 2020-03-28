e92428d9825606e91a3a5da08bd4882688430dc3
FarzamAlam <farzamcse@gmail.com>
2020-03-28 10:59:11 +0530

    Implemented sitemap builder and created a sitemap.xml

--- go-sitemap-builder/main.go
+++ go-sitemap-builder/main.go
@@ -1,27 +1,74 @@
 package main
 
 import (
+	"encoding/xml"
 	"flag"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 
 	"github.com/farzamalam/gopher-exercises/go-link-parser/links"
 )
 
+const xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9"
+
+type loc struct {
+	Value string `xml:"loc"`
+}
+
+type urlset struct {
+	Urls  []loc  `xml:"url"`
+	Xmlns string `xml:"xmlns,attr"`
+}
+
 func main() {
 	// Created a flag url with default "https://www.gophercises.com"
 	urlFlag := flag.String("url", "https://gophercises.com", "the url to make sitemap for.")
+	maxDepth := flag.Int("depth", 3, "maximum depth of search")
 	flag.Parse()
-	fmt.Println(*urlFlag)
+	hrefs := bfs(*urlFlag, *maxDepth)
 
-	hrefs := get(*urlFlag)
-
-	for i, href := range hrefs {
-		fmt.Printf("%d :  %s\n", i, href)
+	toXml := urlset{
+		Xmlns: xmlns,
 	}
+
+	for _, href := range hrefs {
+		toXml.Urls = append(toXml.Urls, loc{href})
+	}
+	fmt.Print(xml.Header)
+	enc := xml.NewEncoder(os.Stdout)
+	enc.Indent("", " ")
+	if err := enc.Encode(toXml); err != nil {
+		panic(err)
+	}
+}
+
+func bfs(urlStr string, maxDepth int) []string {
+	seen := make(map[string]struct{})
+	var q map[string]struct{}
+	nq := map[string]struct{}{
+		urlStr: struct{}{},
+	}
+	for i := 0; i <= maxDepth; i++ {
+		q, nq = nq, make(map[string]struct{})
+		for url := range q {
+			if _, ok := seen[url]; ok {
+				continue
+			}
+			seen[url] = struct{}{}
+			for _, link := range get(url) {
+				nq[link] = struct{}{}
+			}
+		}
+	}
+	ret := make([]string, 0, len(seen))
+	for u := range seen {
+		ret = append(ret, u)
+	}
+	return ret
 }
 
 func get(urlStr string) []string {
