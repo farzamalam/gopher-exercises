bf2321ea336d86c4a67fccaef65960aa68d6f01c
FarzamAlam <farzamcse@gmail.com>
2020-03-29 13:28:13 +0530

    Finished add and list command

--- task/cmd/add.go
+++ task/cmd/add.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"strings"
+
+	"github.com/farzamalam/gopher-exercises/task/db"
 
 	"github.com/spf13/cobra"
 )
@@ -12,6 +15,11 @@ var addCmd = &cobra.Command{
 	Short: "add is used to add new task",
 	Run: func(cmd *cobra.Command, args []string) {
 		task := strings.Join(args, " ")
+		_, err := db.CreateTask(task)
+		if err != nil {
+			fmt.Println("Something went wrong.", err.Error())
+			os.Exit(1)
+		}
 		fmt.Printf("New task \"%s\" has been added.\n", task)
 	},
 }
--- task/cmd/list.go
+++ task/cmd/list.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/farzamalam/gopher-exercises/task/db"
 	"github.com/spf13/cobra"
 )
 
@@ -14,6 +16,18 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "list is used to print all the tasks.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Error while getting the tasks : ", err.Error())
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("No task in the bucket to complete!")
+			return
+		}
+		fmt.Printf("To do list :\n")
+		for i, task := range tasks {
+			fmt.Printf("%d. %s\n", i+1, task.Value)
+		}
 	},
 }
