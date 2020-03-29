16787c243eeab5226e60384948dc90663cfdf4be
FarzamAlam <farzamcse@gmail.com>
2020-03-29 12:34:40 +0530

    Created task.db in home dir, it will be our app db

--- task/main.go
+++ task/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"log"
+	"path/filepath"
+
 	"github.com/farzamalam/gopher-exercises/task/cmd"
+	"github.com/farzamalam/gopher-exercises/task/db"
+	"github.com/mitchellh/go-homedir"
 )
 
 func main() {
+	home, err := homedir.Dir()
+	if err != nil {
+		log.Fatal("Error while getting the home dir.")
+	}
+	dbPath := filepath.Join(home, "tasks.db")
+	err = db.Init(dbPath)
+	if err != nil {
+		log.Fatal("Error while Init() db")
+	}
 	cmd.RootCmd.Execute()
 }
