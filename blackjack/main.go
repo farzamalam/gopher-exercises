c906114e89dcefbf4829f482fc54ecbf9d758a30
FarzamAlam <farzamcse@gmail.com>
2020-04-01 19:36:52 +0530

    Added a dealer and player, and can hit and stand as per player input

--- blackjack/main.go
+++ blackjack/main.go
@@ -10,10 +10,28 @@ import (
 func main() {
 	cards := deck.New(deck.Deck(3), deck.Shuffle)
 	var card deck.Card
-	for i := 0; i < 10; i++ {
-		card, cards = draw(cards)
-		fmt.Println(card)
+	var player, dealer Hand
+	for i := 0; i < 2; i++ {
+		for _, hand := range []*Hand{&player, &dealer} {
+			card, cards = draw(cards)
+			*hand = append(*hand, card)
+		}
 	}
+
+	var input string
+	for input != "s" {
+		fmt.Println("Player : ", player)
+		fmt.Println("Dealer : ", dealer.dealerString())
+		fmt.Println("What will you do? (h)it or (s)tand")
+		fmt.Scanf("%s\n", &input)
+		switch input {
+		case "h":
+			card, cards = draw(cards)
+			player = append(player, card)
+		}
+	}
+	fmt.Println("Dealer : ", dealer)
+	fmt.Println("Player : ", player)
 }
 
 func draw(cards []deck.Card) (deck.Card, []deck.Card) {
@@ -29,4 +47,8 @@ func (h Hand) String() string {
 		strs[i] = h[i].String()
 	}
 	return strings.Join(strs, ", ")
+}
+
+func (h Hand) dealerString() string {
+	return h[0].String() + ", ** Hidden **"
 }
