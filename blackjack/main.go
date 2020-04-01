d10cb82bc4b48300da3ed1d39e87d17fe69dbc7a
FarzamAlam <farzamcse@gmail.com>
2020-04-01 19:23:52 +0530

    wrote draw func and String()

--- blackjack/main.go
+++ blackjack/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/farzamalam/gopher-exercises/deck"
+)
 
 func main() {
-	fmt.Println("Into blackjack.")
+	cards := deck.New(deck.Deck(3), deck.Shuffle)
+	var card deck.Card
+	for i := 0; i < 10; i++ {
+		card, cards = draw(cards)
+		fmt.Println(card)
+	}
+}
+
+func draw(cards []deck.Card) (deck.Card, []deck.Card) {
+	card, cards := cards[0], cards[1:]
+	return card, cards
+}
+
+type Hand []deck.Card
+
+func (h Hand) String() string {
+	strs := make([]string, len(h))
+	for i := range h {
+		strs[i] = h[i].String()
+	}
+	return strings.Join(strs, ", ")
 }
