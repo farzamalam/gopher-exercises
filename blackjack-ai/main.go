7cd1b1e6698a2d22bb6696887fa65ce519d50deb
FarzamAlam <farzamcse@gmail.com>
2020-04-02 16:43:21 +0530

    Implemented Counting in main.go

--- blackjack-ai/main.go
+++ blackjack-ai/main.go
@@ -3,8 +3,55 @@ package main
 import (
 	"fmt"
 
+	"github.com/farzamalam/gopher-exercises/deck"
+
 	"github.com/farzamalam/gopher-exercises/blackjack-ai/blackjack"
 )
+
+type basicAI struct {
+	score int
+	seen  int
+	decks int
+}
+
+func (ai *basicAI) Bet(shuffled bool) int {
+	if shuffled {
+		ai.score = 0
+		ai.seen = 0
+	}
+	trueScore := ai.score / ((ai.decks*52 - ai.seen) / 52)
+	switch {
+	case trueScore >= 14:
+		return 100000
+	case trueScore >= 8:
+		return 5000
+	default:
+		return 100
+	}
+}
+
+func (ai *basicAI) Play(hand []deck.Card, dealer deck.Card) blackjack.Move {
+	score := blackjack.Score(hand...)
+	if len(hand) == 2 {
+		if hand[0] == hand[1] {
+			cardScore := blackjack.Score(hand[0])
+			if cardScore >= 8 && cardScore != 10 {
+				return blackjack.MoveSplit
+			}
+		}
+		if (score == 10 || score == 11) && !blackjack.Soft(hand...) {
+			return blackjack.MoveDouble
+		}
+	}
+	dScore := blackjack.Score(dealer)
+	if dScore >= 5 && dScore <= 6 {
+		return blackjack.MoveStand
+	}
+	if score < 13 {
+		return blackjack.MoveHit
+	}
+	return blackjack.MoveStand
+}
 
 func main() {
 	opts := blackjack.Options{
