8ef20d2c3c36178040015c60d01376d089cc7084
FarzamAlam <farzamcse@gmail.com>
2020-04-07 13:24:37 +0530

    implemented recover and rendered code in browser

--- recover/main.go
+++ recover/main.go
@@ -9,9 +9,12 @@ import (
 	"net/url"
 	"os"
 	"runtime/debug"
+	"strconv"
 	"strings"
 
-	"github.com/alecthomas/chroma/quick"
+	"github.com/alecthomas/chroma/formatters/html"
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/styles"
 )
 
 func main() {
@@ -50,8 +53,14 @@ func recoverMW(app http.Handler, dev bool) http.HandlerFunc {
 // 	Write([]byte) (int, error)
 // 	WriteHeader(statusCode int)
 // }
+
 func sourceCodeHandler(w http.ResponseWriter, r *http.Request) {
 	path := r.FormValue("path")
+	lineStr := r.FormValue("line")
+	line, err := strconv.Atoi(lineStr)
+	if err != nil {
+		line = -1
+	}
 	file, err := os.Open(path)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -63,34 +72,21 @@ func sourceCodeHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	//err = quick.Highlight(os.Stdout, "package main", "go", "html", "monokai")
-	err = quick.Highlight(w, b.String(), "go", "html", "github")
-}
-
-func makeLinks(stack string) string {
-	lines := strings.Split(stack, "\n")
-	for li, line := range lines {
-		if len(line) == 0 || line[0] != '\t' {
-			continue
-		}
-		var file string
-		count := 0
-		for i, ch := range line {
-			if ch == ':' {
-				count++
-				if count == 2 {
-					file = line[1:i]
-					count = 0
-					break
-				}
-
-			}
-		}
-		v := url.Values{}
-		v.Set("path", file)
-		lines[li] = "\t <a href = \"/debug/?" + v.Encode() + "\">" + file + "</a>" + line[len(file)+1:]
+	var lines [][2]int
+	if line > 0 {
+		lines = append(lines, [2]int{line, line})
 	}
-	return strings.Join(lines, "\n")
+	lexer := lexers.Get("go")
+	iterator, err := lexer.Tokenise(nil, b.String())
+	style := styles.Get("github")
+	if style == nil {
+		style = styles.Fallback
+	}
+	formatter := html.New(html.TabWidth(2), html.HighlightLines(lines))
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<style>pre { font-size: 1.2em; }</style>")
+	formatter.Format(w, style, iterator)
+	// _ = quick.Highlight(w, b.String(), "go", "html", "github")
 }
 
 type responseWriter struct {
@@ -135,4 +131,37 @@ func funcThatPanic() {
 func panicAfterDemo(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello !</h1>")
 	funcThatPanic()
+}
+
+func makeLinks(stack string) string {
+	lines := strings.Split(stack, "\n")
+	for li, line := range lines {
+		if len(line) == 0 || line[0] != '\t' {
+			continue
+		}
+		file := ""
+		count := 0
+		for i, ch := range line {
+			if ch == ':' {
+				count++
+				if count == 2 {
+					file = line[1:i]
+					count = 0
+					break
+				}
+			}
+		}
+		var lineStr strings.Builder
+		for i := len(file) + 2; i < len(line); i++ {
+			if line[i] < '0' || line[i] > '9' {
+				break
+			}
+			lineStr.WriteByte(line[i])
+		}
+		v := url.Values{}
+		v.Set("path", file)
+		v.Set("line", lineStr.String())
+		lines[li] = "\t<a href=\"/debug/?" + v.Encode() + "\">" + file + ":" + lineStr.String() + "</a>" + line[len(file)+2+len(lineStr.String()):]
+	}
+	return strings.Join(lines, "\n")
 }
