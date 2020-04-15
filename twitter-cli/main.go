4a730f0876085d8fb870f94ddea9cfe9096fb89b
FarzamAlam <farzamcse@gmail.com>
2020-04-15 21:43:28 +0530

    Initialized slck repo, and created command type and constants Op

--- twitter-cli/main.go
+++ twitter-cli/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
@@ -12,47 +13,67 @@ import (
 )
 
 func main() {
+
+	keyFile := flag.String("key", "keys.json", "The file that contains the key and secret")
+	tweetID := flag.String("tweet", "1221142432177221634", "The tweet id that is used to get the information.")
+	userFile := flag.String("users", "users.csv", "The file that contains the usernames of all the people who retweeted.")
+	_ = userFile
+	flag.Parse()
+
+	key, secret, err := keys(*keyFile)
+	if err != nil {
+		panic(err)
+	}
+	client, err := twitterClient(key, secret)
+	if err != nil {
+		panic(err)
+	}
+	usernames, err := retweeters(client, *tweetID)
+	fmt.Println(usernames)
+	fmt.Println("len : ", len(usernames))
+}
+
+func keys(keyFile string) (key, secret string, err error) {
 	var keys struct {
 		Key    string `json:"consumer_key"`
 		Secret string `json:"consumer_secret"`
 	}
-	f, err := os.Open("keys.json")
+	f, err := os.Open(keyFile)
 	if err != nil {
-		panic(err)
+		return "", "", nil
 	}
 	defer f.Close()
 	d := json.NewDecoder(f)
 	d.Decode(&keys)
-	//fmt.Printf("%+v\n", keys)
+	return keys.Key, keys.Secret, nil
+}
 
+func twitterClient(key, secret string) (*http.Client, error) {
 	req, err := http.NewRequest("POST", "https://api.twitter.com/oauth2/token", strings.NewReader("grant_type=client_credentials"))
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	req.SetBasicAuth(keys.Key, keys.Secret)
+	req.SetBasicAuth(key, secret)
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
 	var client http.Client
 	res, err := client.Do(req)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	defer res.Body.Close()
 	//io.Copy(os.Stdout, res.Body)
 
 	var token oauth2.Token
-	d = json.NewDecoder(res.Body)
+	d := json.NewDecoder(res.Body)
 	err = d.Decode(&token)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	//fmt.Println("Token ", token)
 
 	ctx := context.Background()
 	var conf oauth2.Config
-	tclient := conf.Client(ctx, &token)
-	usernames, err := retweeters(tclient, "1221142432177221634")
-	fmt.Println(usernames)
-	fmt.Println("len : ", len(usernames))
+	return conf.Client(ctx, &token), nil
 }
 
 func retweeters(client *http.Client, tweetID string) ([]string, error) {
