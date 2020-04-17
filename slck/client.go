087efa5e0f30abd75660866f0cdc2c345619eba1
FarzamAlam <farzamcse@gmail.com>
2020-04-17 08:33:07 +0530

    Added reg, join, leave and msg methods for client

--- slck/client.go
+++ slck/client.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"strconv"
 )
 
 var (
@@ -44,7 +45,7 @@ func (c *client) read() error {
 	return nil
 }
 
-func handle(message []byte) {
+func (c *client) handle(message []byte) {
 	cmd := bytes.ToUpper(bytes.TrimSpace(bytes.Split(message, []byte(" "))[0]))
 	args := bytes.TrimSpace(bytes.TrimPrefix(message, cmd))
 	switch string(cmd) {
@@ -71,4 +72,73 @@ func handle(message []byte) {
 	default:
 		c.err(fmt.Errorf("Unknown command %s", cmd))
 	}
+}
+
+func (c *client) reg(args []byte) error {
+	u := bytes.TrimSpace(args)
+	if u[0] != '@' {
+		return fmt.Errorf("Username must begin with @")
+	}
+	if len(u) == 0 {
+		return fmt.Errorf("Username cannot be blank.")
+	}
+	c.username = string(u)
+	c.register <- c
+	return nil
+}
+
+func (c *client) join(args []byte) error {
+	channelID := bytes.TrimSpace(args)
+	if channelID[0] != '#' {
+		return fmt.Errorf("Channel ID must begin with #")
+	}
+	c.outbound <- command{
+		recipient: string(channelID),
+		sendor:    c.username,
+		id:        JOIN,
+	}
+	return nil
+}
+
+func (c *client) leave(args []byte) error {
+	channelID := bytes.TrimSpace(args)
+	if channelID[0] != '#' {
+		return fmt.Errorf("Channel ID must begin with #")
+	}
+	c.outbound <- command{
+		recipient: string(channelID),
+		sendor:    c.username,
+		id:        LEAVE,
+	}
+	return nil
+}
+
+// Bug in the code of fteem
+func (c *client) msg(args []byte) error {
+	args = bytes.TrimSpace(args)
+	if args[0] != '#' || args[0] != '@' {
+		return fmt.Errorf("Recipient must be a channel(#) or user(@)")
+	}
+	recipient := bytes.Split(args, []byte(" "))[0]
+	if len(recipient) == 0 {
+		return fmt.Errorf("Recipient must have a name")
+	}
+	args = bytes.TrimSpace(bytes.TrimPrefix(args, recipient))
+	l := bytes.Split(args, DELIMITER)[0]
+	length, err := strconv.Atoi(string(l))
+	if err != nil {
+		return fmt.Errorf("Body length must be present")
+	}
+	if length == 0 {
+		return fmt.Errorf("Body lenght must be atleast 1")
+	}
+	padding := len(l) + len(DELIMITER)
+	body := args[padding : padding+length]
+	c.outbound <- command{
+		recipient: string(recipient),
+		sendor:    c.username,
+		body:      body,
+		id:        MSG,
+	}
+	return nil
 }
