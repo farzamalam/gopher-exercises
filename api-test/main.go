9b55d213e925ea926cb3deb9fba0c595cd68fcb8
FarzamAlam <farzamcse@gmail.com>
2020-05-11 13:58:10 +0530

    Added a handlers for basic http methods in ServeHTTP

--- api-test/main.go
+++ api-test/main.go
@@ -9,8 +9,24 @@ type server struct{}
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message":"Hello world!"}`))
+
+	switch r.Method {
+	case "GET":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message":"Get called!"}`))
+	case "POST":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message":"Post called!"}`))
+	case "PUT":
+		w.WriteHeader(http.StatusAccepted)
+		w.Write([]byte(`{"message":"Put called!"}`))
+	case "DELETE":
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message":"Delete called!"}`))
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"message": "Not found"}`))
+	}
 }
 
 func main() {
