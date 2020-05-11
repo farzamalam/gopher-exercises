2b365083a8f1c5a1809c383877ebea012480b043
FarzamAlam <farzamcse@gmail.com>
2020-05-11 16:53:18 +0530

    Added a subrouter

--- api-test/main.go
+++ api-test/main.go
@@ -10,33 +10,20 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
-
-	switch r.Method {
-	case "GET":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message":"Get called!"}`))
-	case "POST":
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(`{"message":"Post called!"}`))
-	case "PUT":
-		w.WriteHeader(http.StatusAccepted)
-		w.Write([]byte(`{"message":"Put called!"}`))
-	case "DELETE":
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"message":"Delete called!"}`))
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte(`{"message": "Not found"}`))
-	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Welcome Home!"))
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", handler.Get).Methods(http.MethodGet)
-	r.HandleFunc("/", handler.Post).Methods(http.MethodPost)
-	r.HandleFunc("/", handler.Put).Methods(http.MethodPut)
-	r.HandleFunc("/", handler.Delete).Methods(http.MethodDelete)
-	r.HandleFunc("/", handler.NotFound)
+	// Added a sub router to make provisions for multiple versions.
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("", handler.Get).Methods(http.MethodGet)
+	api.HandleFunc("", handler.Post).Methods(http.MethodPost)
+	api.HandleFunc("", handler.Put).Methods(http.MethodPut)
+	api.HandleFunc("", handler.Delete).Methods(http.MethodDelete)
+	api.HandleFunc("", handler.NotFound)
+	r.HandleFunc("/", home)
 	log.Println("Starting server at : 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
