ec707469601e5e491fe8d787854e628879393284
FarzamAlam <farzamcse@gmail.com>
2020-05-12 17:38:03 +0530

    Wrote update, delete and searchISBN method

--- bookdata/main.go
+++ bookdata/main.go
@@ -1,16 +1,25 @@
 package main
 
-import "github.com/farzamalam/gopher-exercises/bookdata/model"
+import (
+	"github.com/farzamalam/gopher-exercises/bookdata/model"
+)
 
 // TO DO
 // 0. Read and understand models, operations, urls and handler funcs  --> Done.
-// 1. Make model and data object to hold data.
+// 1. Make model and data object to hold data.  --> Done.
 // 2. Implement operations on slice of books
 // 3. Write urls and query paramters.
 // 4. Implement all the handler funcs, and exopose operations
+// 5. Deploy on Nginx Server.
+
+var books model.BookStore
+
 func init() {
-	model.Initialize()
+	books = &model.Books{}
+	books.Initialize()
+
 }
 
 func main() {
+	books.PrintBooks()
 }
--- bookdata/model/books.go
+++ bookdata/model/books.go
@@ -4,19 +4,91 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
+
+type BookStore interface {
+	Initialize()
+	PrintBooks()
+	//SearchAuthor(author string, ratingOver, ratingBelow float64, limit, skip int) *[]*Book
+	//SearchBook(bookName string, ratingOver, ratingBelow float64, limit, skip int) *[]*Book
+	SearchISBN(isbn string) *Book
+	CreateBook(book *Book) bool
+	DeleteBook(isbn string) bool
+	UpdateBook(isbn string, book *Book) bool
+}
+
+type Books struct {
+	Store *[]*Book
+}
 
 // Initialize opens the csv and calls the loadData to read the csv and gets a book that contains
 // sile of pointer to book
-func Initialize() {
+func (b *Books) Initialize() {
 	fileName := "books.csv"
 	file, err := os.Open(fileName)
 	defer file.Close()
 	if err != nil {
 		log.Fatalf("Error while opening the file %s\n", fileName)
 	}
-	books := loadData(file)
+	b.Store = loadData(file)
+}
+
+func (b *Books) UpdateBook(isbn string, book *Book) bool {
+	for _, bk := range *b.Store {
+		if bk.ISBN == isbn {
+			bk = book
+			return true
+		}
+	}
+	return false
+}
+
+func (b *Books) CreateBook(book *Book) bool {
+	*b.Store = append(*b.Store, book)
+	return true
+}
+
+func (b *Books) DeleteBook(isbn string) bool {
+	idx := -1
+	for i, bk := range *b.Store {
+		if bk.ISBN == isbn {
+			idx = i
+			break
+		}
+	}
+	if idx == -1 {
+		return false
+	}
+	res := (*b.Store)[:idx]
+	res = append(res, (*b.Store)[idx+1:]...)
+	*b.Store = res
+	return true
+}
+
+func (b Books) PrintBooks() {
+	books := b.Store
 	for _, book := range *books {
 		fmt.Println(*book)
 	}
+}
+
+func (b *Books) SearchISBN(isbn string) *Book {
+	res := Filter(b.Store, func(book *Book) bool {
+		return strings.ToLower(book.ISBN) == strings.ToLower(isbn)
+	})
+	if len(*res) > 0 {
+		return (*res)[0]
+	}
+	return nil
+}
+
+func Filter(books *[]*Book, f func(*Book) bool) *[]*Book {
+	var res []*Book
+	for _, book := range *books {
+		if f(book) {
+			res = append(res, book)
+		}
+	}
+	return &res
 }
