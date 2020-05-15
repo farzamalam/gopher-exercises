3030f6d3f4f5fffac4c6873f01e41debc41f247f
FarzamAlam <farzamcse@gmail.com>
2020-05-15 23:44:47 +0530

    Updated contact.go and gitignore

--- contacts/models/contacts.go
+++ contacts/models/contacts.go
@@ -2,9 +2,9 @@ package models
 
 // Contact type is used to store single contacts in contacts table.
 type Contact struct {
-	ContactsID uint   `json:"contacts_id"`
-	Name       string `json:"name"`
-	Phone      string `json:"phone"`
-	UserID     uint   `json:"user_id"`
-	CreatedID  string `json:"created_at"`
+	ContactID uint   `json:"contact_id"`
+	Name      string `json:"name"`
+	Phone     string `json:"phone"`
+	UserID    uint   `json:"user_id"`
+	CreatedID string `json:"created_at"`
 }
