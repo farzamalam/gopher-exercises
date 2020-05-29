52910ccd6385075520ab1a364a6b40e0d1c71733
FarzamAlam <farzamcse@gmail.com>
2020-05-29 13:39:10 +0530

    Pingged into redis server without docker

--- visits/go.mod
+++ visits/go.mod
@@ -2,7 +2,4 @@ module github.com/farzamalam/gopher-exercises/visits
 
 go 1.13
 
-require (
-	github.com/go-redis/redis v6.15.8+incompatible
-	github.com/go-redis/redis/v8 v8.0.0-beta.2 // indirect
-)
+require github.com/go-redis/redis v6.15.8+incompatible
--- visits/main.go
+++ visits/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/go-redis/redis"
 )
 
 func main() {
-	ctx := context.Background()
 	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "192.168.99.100:6379",
 		Password: "",
 		DB:       0,
 	})
