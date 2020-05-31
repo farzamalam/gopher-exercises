44787add5f33e075da3b257a90cc097a2eadc41a
FarzamAlam <farzamcse@gmail.com>
2020-05-31 00:06:15 +0000

    Added util funcs Message and Respond in the utils package

---
+++ contacts/utils/util.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Message is used to set the status and message in the response
+func Message(status bool, message string) map[string]interface{} {
+	return map[string]interface{}{"status": status, "message": message}
+}
+
+// Respond is used to write the message and statusCode and Content-type to the response.
+func Respond(w http.ResponseWriter, statusCode int, data map[string]interface{}) {
+	w.Header().Add("Content-type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(data)
+}
