bd303d5b67051e9bdb36492e0778a4f756ecf51c
FarzamAlam <farzamcse@gmail.com>
2020-06-01 04:24:28 +0000

    Added Create() and Validate() account funcs

--- contacts/models/account.go
+++ contacts/models/account.go
@@ -1,10 +1,76 @@
 package models
 
 import (
+	"database/sql"
+	"os"
+	"strings"
+
 	"github.com/dgrijalva/jwt-go"
+	"github.com/farzamalam/gopher-exercises/contacts/utils"
 )
 
 type Token struct {
 	UserID int
 	jwt.StandardClaims
+}
+
+type Account struct {
+	AccountsID int    `json:"accounts_id"`
+	Name       string `json:"name"`
+	Email      string `json:"email"`
+	Password   string `json:"password"`
+	Token      string `json:"token";sql:"-"`
+	CreatedAt  string `json:"created_at"`
+}
+
+// Validate incoming user details.
+func (account *Account) Validate(map[string]interface{}, bool) {
+	if !strings.Contains(account.Email, "@") {
+		return utils.Message(false, "Email address is required"), false
+	}
+	if len(account.Password) < 6 {
+		return utils.Message(false, "Valid Password is required"), false
+	}
+	// Email must be unique
+	_, err := GetDB().Query("Select accounts_id from accounts where email = ?", account.Email)
+	if err != sql.ErrNoRows {
+		return utils.Message(false, "Connection Error. Please retry"), false
+	}
+	if err == nil {
+		return utils.Message(false, "Email address is already present."), false
+	}
+	return utils.Message(false, "Requirement Passed."), true
+}
+
+// Create new account.
+func (account *Account) Create() map[string]interface{} {
+	if resp, ok := account.Validate(); !ok {
+		return resp
+	}
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(account.Password), bcrypt.DefaultCost)
+	account.Password = string(hashedPassword)
+
+	sql := `
+		Insert into accounts (name, email, password)
+		values(?,?,?);
+	`
+	_, err := GetDB().Query(sql, account.Name, account.Email, account.Password)
+	if err != nil {
+		return utils.Message(false, "Error while Inserting the account")
+	}
+	sql = "select * from accounts where accounts_id= (select max(accounts_id) from accounts)"
+	err = GetDB().Query(sql).Scan(&account.AccountsID, &account.Name, &account.Email, &account.Password, &account.Token, &account.CreatedAt)
+	if err != nil {
+		return utils.Message(false, "Error while getting the Account details after insert.")
+	}
+
+	// Create a new JTW token for the newly registered account.
+	tk := &Token{UserID: account.AccountsID}
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tk)
+	tokenString, _ := token.SignedString([]byte(os.Getenv("token_password")))
+	account.Token = tokenString
+	account.Password = ""
+	resp := utils.Message(true, "Account has been created.")
+	resp["data"] = account
+	return resp
 }
