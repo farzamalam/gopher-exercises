1ac209ef4122e57e385ddbafc0631d63b362c3f4
Farzam Alam <farzamcse@gmail.com>
2020-06-05 22:56:37 +0530

    Update main.go

--- api-test/main.go
+++ api-test/main.go
@@ -16,6 +16,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	r := mux.NewRouter()
+	port := "8080"
 	// Added a sub router to make provisions for multiple versions.
 	api := r.PathPrefix("/api/v1").Subrouter()
 	api.HandleFunc("", handler.Get).Methods(http.MethodGet)
@@ -25,6 +26,6 @@ func main() {
 	api.HandleFunc("", handler.NotFound)
 	api.HandleFunc("/user/{userID}/comment/{commentID}", handler.Params).Methods(http.MethodGet)
 	r.HandleFunc("/", home)
-	log.Println("Starting server at : 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Println("Starting server at : ",port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
