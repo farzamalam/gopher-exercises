0256fdda4975e61d07f8830735b32f9f302197e3
Farzam Alam <farzamcse@gmail.com>
2020-06-13 23:52:57 +0530

    Removed unnecessory comments.

--- bookdata/main.go
+++ bookdata/main.go
@@ -8,15 +8,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// TO DO
-// 0. Read and understand models, operations, urls and handler funcs  --> Done.
-// 1. Make model and data object to hold data.	--> Done.
-// 2. Implement operations on slice of books. --> Done.
-// 3. Write urls and query paramters.	--> Done.
-// 4. Implement all the handler funcs, and expose operations.	--> Done.
-// 5. Test on localhost.	--> Done.
-// 5. Deploy on Server.	--> Done.
-
 func main() {
 	r := mux.NewRouter()
 	api := r.PathPrefix("/api/v1").Subrouter()
