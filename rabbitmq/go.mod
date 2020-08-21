aa0a86860d99f383ff541fd8d54d10b26c6fa90d
FarzamAlam <farzamcse@gmail.com>
2020-08-21 14:08:36 +0530

    Completed RabbitMQ tutorial

--- rabbitmq/go.mod
+++ rabbitmq/go.mod
@@ -2,4 +2,7 @@ module github.com/farzamalam/gopher-exercises/rabbitmq
 
 go 1.13
 
-require github.com/streadway/amqp v1.0.0 // indirect
+require (
+	github.com/alecthomas/chroma v0.8.0 // indirect
+	github.com/streadway/amqp v1.0.0 // indirect
+)
--- rabbitmq/rpc-client.go
+++ rabbitmq/rpc-client.go
@@ -5,9 +5,10 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
-	"github.com/alecthomas/chroma/lexers/q"
+	"github.com/streadway/amqp"
 )
 
 func failOnError(err error, msg string) {
@@ -25,11 +26,11 @@ func main() {
 	log.Printf(" [.] Got %d", res)
 }
 
-func bodyFrom(args []string)int  {
+func bodyFrom(args []string) int {
 	var s string
-	if len(args) < || os.Args[1] == ""{
+	if len(args) < 2 || os.Args[1] == "" {
 		s = "30"
-	}else{
+	} else {
 		s = strings.Join(args[1:], " ")
 	}
 	n, err := strconv.Atoi(s)
@@ -37,7 +38,7 @@ func bodyFrom(args []string)int  {
 	return n
 }
 
-func fioonacciRPC(n int)(res int, err error)  {
+func fibonacciRPC(n int) (res int, err error) {
 	conn, err := amqp.Dial("amqp://guest:guest@192.168.99.100:5672")
 	failOnError(err, "Failed to Connect to RabbitMQ.")
 	defer conn.Close()
@@ -69,22 +70,23 @@ func fioonacciRPC(n int)(res int, err error)  {
 	corrId := randomString(32)
 
 	err = ch.Publish(
-		"", // Exchange
+		"",          // Exchange
 		"rpc_queue", // routing key
 		false,
 		false,
 		amqp.Publishing{
-			ContentType: "text/plain",
-			CorrelationId : corrId,
-			ReplyTo : q.Name,
-			Body : []byte(strconv.Itoa(n))
-		}
+			ContentType:   "text/plain",
+			CorrelationId: corrId,
+			ReplyTo:       q.Name,
+			Body:          []byte(strconv.Itoa(n)),
+		},
 	)
 	failOnError(err, "Failed to publish a message.")
 
-	for d  := range msgs{
-		if corrId == d.CorrelationId{
+	for d := range msgs {
+		if corrId == d.CorrelationId {
 			res, err := strconv.Atoi(string(d.Body))
+			log.Println("res  : ", res)
 			failOnError(err, "Failed to convert body to integer.")
 			break
 		}
@@ -95,7 +97,7 @@ func fioonacciRPC(n int)(res int, err error)  {
 func randomString(l int) string {
 	bytes := make([]byte, l)
 	for i := 0; i < l; i++ {
-			bytes[i] = byte(randInt(65, 90))
+		bytes[i] = byte(randInt(65, 90))
 	}
 	return string(bytes)
 }
