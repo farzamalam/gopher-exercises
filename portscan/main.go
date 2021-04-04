d29348086f8fff5e50bd2f12428d0ce3836872ca
FarzamAlam <farzamcse@gmail.com>
2021-04-04 09:43:07 +0530

    Used waitgroup to fix issue of concurrency

--- portscan/main.go
+++ portscan/main.go
@@ -1,26 +1,56 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
+	"strconv"
+	"sync"
 )
 
+var host string
+var fromPort string
+var toPort string
+
+func init() {
+	flag.StringVar(&host, "host", "localhost", "host to scan.")
+	flag.StringVar(&fromPort, "from", "8080", "Port to start scanning from.")
+	flag.StringVar(&toPort, "to", "8090", "Port to end scanning to.")
+}
+
 func main() {
-	h := "localhost"
-	fp := 8075
-	tp := 8085
+	flag.Parse()
+	fp, err := strconv.Atoi(fromPort)
+	if err != nil {
+		log.Fatalf("Error while parsing 'from' port: %v", err)
+	}
+	tp, err := strconv.Atoi(toPort)
+	if err != nil {
+		log.Fatalf("Error while parsing 'to' port : %v", err)
+	}
+
+	if fp > tp {
+		log.Fatalf("Invalid values of 'from' and 'to' port.")
+	}
+
+	var wg sync.WaitGroup
+
+	wg.Add(tp - fp + 1)
 	for p := fp; p <= tp; p++ {
 		go func(p int) {
-			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", h, p))
+			defer wg.Done()
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", host, p))
 			if err != nil {
-				log.Printf("%d - CLOSED : %v\n", p, err)
+				log.Printf("%d Closed %s\n", p, err)
 				return
 			}
 			conn.Close()
-			log.Printf("%d - OPEN\n", p)
+			log.Printf("%d Open\n", p)
 		}(p)
-
 	}
-	log.Printf("DONE!\n")
+	log.Println("Waiting")
+	wg.Wait()
+	log.Println("Done.")
+
 }
