8b45a3ef4d4bfa2e89d30d37ec19ff2456417fc6
farzamalam <farzamcse@gmail.com>
2021-07-07 22:12:41 +0530

    Implemented CRUD operation on toml file

--- config-load/go.mod
+++ config-load/go.mod
@@ -4,5 +4,4 @@ go 1.16
 
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
-	github.com/pelletier/go-toml v1.9.3 // indirect
 )
--- config-load/main.go
+++ config-load/main.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"time"
 
-	toml "github.com/pelletier/go-toml"
+	"github.com/BurntSushi/toml"
 )
 
 type Config struct {
@@ -14,57 +13,41 @@ type Config struct {
 	Redis map[string]redis
 }
 
-type app struct {
-	Auther  string
-	Org     string `toml:"organization"`
-	Mark    string
-	Release time.Time
-}
-
-type mysql struct {
-	Server  string
-	Ports   []int
-	ConnMax int `toml:"connection_max"`
-	Enabled bool
-}
-
 type redis struct {
 	Host string
 	Port int
 }
 
-type releases struct {
-	Release []string
-	Tags    [][]interface{}
-}
-
-type Company struct {
-	Name   string
-	Detail detail
-}
-
-type detail struct {
-	Type string
-	Addr string
-	ICP  string
-}
-
 func main() {
+	// config, err := toml.LoadFile(".config")
+	// if err != nil {
+	// 	fmt.Println("error to load file: ", err)
+	// }
 	var config Config
-
 	f, err := os.OpenFile(".config", os.O_RDWR, 0777)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
-	b, err := ioutil.ReadAll(f)
-	if err = toml.Unmarshal(b, &config); err != nil {
+	b, _ := ioutil.ReadAll(f)
+	if err := toml.Unmarshal(b, &config); err != nil {
 		fmt.Println("error: ", err)
 	}
-	fmt.Println("config: ", config)
 	fmt.Println("title: ", config.Title)
-	fmt.Println("master: ", config.Redis["master"].Host)
+
+	config.list()
+	config.add()
+	config.delete()
+	config.update()
+	writeFile(f)
+	err = toml.NewEncoder(f).Encode(config)
+	fmt.Println("redis: ", config.Redis)
+	if err != nil {
+		panic(err)
+	}
+
+	// fmt.Println("master: ", config.Redis["master"].Host)
 
 	// r := redis{Host: "127.0.0.2"}
 	// config.Redis["localhost"] = r
@@ -74,9 +57,41 @@ func main() {
 	// fmt.Println("slave: ", config.Redis["slave-0"])
 	// // r4 := redis{Host: "127.0.0.4"}
 	// // config.Redis["slave-4"] = r4
-	// // err = toml.NewEncoder(f).Encode(config)
-	// fmt.Println("redis: ", config.Redis)
-	// if err != nil {
-	// 	panic(err)
-	// }
+
 }
+func writeFile(f *os.File) {
+	f.Truncate(0)
+	f.Seek(0, 0)
+}
+
+func (c *Config) update() {
+	r := c.Redis["host-1"]
+	r.Port = 4321
+	c.Redis["host-1"] = r
+}
+
+func (c *Config) delete() {
+	delete(c.Redis, "slave-4")
+}
+
+func (c *Config) list() {
+	for k, r := range c.Redis {
+		fmt.Printf("%s: URL: %s:%d\n", k, r.Host, r.Port)
+	}
+}
+
+func (c *Config) add() {
+	r := redis{
+		Host: "192.168.1.1",
+		Port: 1234,
+	}
+	c.Redis["host-1"] = r
+}
+
+// func list(config *toml.Tree) {
+// 	title := config.Get("Title").(string)
+// 	fmt.Println("title: ", title)
+// 	redis := config.Get("Redis").(*toml.Tree)
+// 	r := redis.Delete("localhost")
+// 	fmt.Println("localhost: ", r)
+// }
