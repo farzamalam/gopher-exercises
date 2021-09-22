2909fe18194fbe3a3e3eec538e17b7f67cd04a88
frzam <farzamcse@gmail.com>
2021-09-22 19:28:11 +0530

    auth-service: created create end point

--- auth-service/main.go
+++ auth-service/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type BasicAuthResponse struct {
+	Authenticated bool   `json:"authenticated"`
+	User          string `json:"user"`
+}
+
+type CreateAuth struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type CreateAuthResponse struct {
+	Created bool   `json:"created"`
+	User    string `json:"user"`
+}
+
 func main() {
 
 	http.HandleFunc("/api/v1/verify", verify)
+	http.HandleFunc("/api/v1/create", create)
 	port := "8080"
 	log.Printf("Starting server at : %s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
@@ -24,4 +41,52 @@ func verify(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Printf("Username: %s\n", username)
 	log.Printf("Password: %s\n", password)
+
+	v := verifyInDB(username, password)
+	if v {
+		out := BasicAuthResponse{
+			Authenticated: v,
+			User:          username,
+		}
+		resp, _ := json.Marshal(out)
+		fmt.Fprint(w, string(resp))
+		return
+	}
+
+}
+
+func create(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		fmt.Fprintf(w, "Invalid method\n")
+		return
+	}
+	var b CreateAuth
+
+	err := json.NewDecoder(r.Body).Decode(&b)
+	defer r.Body.Close()
+
+	if err != nil {
+		fmt.Fprintf(w, "Invalid body: %s\n", err)
+		return
+	}
+	log.Printf("Username: %s\n", b.Username)
+	log.Printf("Password: %s\n", b.Password)
+	err = createInDB(b.Username, b.Password)
+	if err != nil {
+		fmt.Fprintf(w, "Internal server Error: %s", err)
+		return
+	}
+	data := CreateAuthResponse{
+		Created: true,
+		User:    b.Username,
+	}
+	resp, _ := json.Marshal(data)
+	fmt.Fprintf(w, "%s\n", resp)
+}
+
+func verifyInDB(username, password string) bool {
+	return false
+}
+func createInDB(username, password string) error {
+	return nil
 }
